0d7a25eb416227c3150a44b2d69450a19f43a3ed
Stephen Bunch <bunchclone@gmail.com>
2015-05-04 10:59:14 -0700

    add babel karam integration

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function( config ) {
       'node_modules/chai/chai.js',
       'node_modules/sinon/pkg/sinon.js',
       'node_modules/sinon-chai/lib/sinon-chai.js',
+      'node_modules/babel/node_modules/babel-core/browser-polyfill.js',
       'test/_karma.js',
 
       'dist/' + pkg.name + '.js',
@@ -18,14 +19,29 @@ module.exports = function( config ) {
       'test/**/*.spec.js'
     ],
 
-    reporters: [ "progress" ],
+    reporters: [ 'progress' ],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
     singleRun: false,
 
-    browsers: [ "Chrome" ]
+    browsers: [ 'Chrome' ],
 
+    preprocessors: {
+      'test/**/*.js': [ 'babel' ]
+    },
+
+    babelPreprocessor: {
+      options: {
+        sourceMap: 'inline'
+      },
+      filename: function( file ) {
+        return file.originalPath.replace( /\.js$/, '.es5.js' );
+      },
+      sourceFileName: function( file ) {
+        return file.originalPath;
+      }
+    }
   });
 };
